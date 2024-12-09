76ca47ec3a90ace95e8e077dfcc8309ae74fe0d2
Джером <jorbinarojr@gmail.com>
2024-12-09 10:07:56 +0300

    First Commit/Add class Note

---
+++ back/note.py
@@ -0,0 +1,9 @@
+import datetime
+
+class Note:
+    def __init__(self, id, title: str, content: str, category):
+        self.id = id
+        self.title = title
+        self.content = content
+        self.date = datetime.datetime.now()
+        self.category = category
